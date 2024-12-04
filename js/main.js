835633bbf5fa94b0852e638ffafc2a391f401365
vvvryyi <124810205+vvvryyi@users.noreply.github.com>
2024-12-04 17:09:57 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,14 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.querySelector('.hamburger');
     const navLinks = document.querySelector('.nav__links');
-    const navLogo = document.querySelector('.nav__logo')
-    
+    const navButtons = document.querySelector('.nav__buttons');
+    const navLogo = document.querySelector('.nav__logo');
+    // Функция закрытия меню
+    function closeMenu() {
+        hamburger.classList.remove('active');
+        navLinks.classList.remove('active');
+        navLogo.classList.remove('active');
+        navButtons.classList.remove('active');
+    }
+
+    // Открытие/закрытие меню при клике на гамбургер
     hamburger.addEventListener('click', function() {
-        // Переключаем классы active для меню и гамбургера
         hamburger.classList.toggle('active');
         navLinks.classList.toggle('active');
-        navLogo.classList.toggle('active')
+        navLogo.classList.toggle('active');
+        navButtons.classList.toggle('active');
     });
 
+    // Закрытие при клике на ссылки
+    navLinks.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', closeMenu);
+    });
+
+    // Закрытие при клике на кнопки
+    navButtons.querySelectorAll('button').forEach(button => {
+        button.addEventListener('click', closeMenu);
+    });
+
+    navLogo.addEventListener('click', closeMenu);
 
 });
